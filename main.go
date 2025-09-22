d4aac1ce831a3841a08105cbf6005a7ad00ff935
Jinminzuo <110321926+Jinminzuo@users.noreply.github.com>
2025-09-22 11:48:58 +0800

    feat
    commit

--- main.go
+++ main.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"log"
 	"net/http"
+	"wxcloudrun-golang/service"
 )
 
-// JsonResult 返回结构
-type JsonResult struct {
-	Code     int         `json:"code"`
-	ErrorMsg string      `json:"errorMsg,omitempty"`
-	Data     interface{} `json:"data"`
-}
-
 func main() {
-	// 创建 Echo 实例
-	e := echo.New()
 
-	// 注册 /helloworld 路由
-	e.GET("/helloworld", HelloWorldHandler)
+	http.HandleFunc("/", service.HelloWorldHandler)
 
-	// 启动服务，监听 8080
-	e.Logger.Fatal(e.Start(":80"))
-}
-
-// HelloWorldHandler 返回 hello world JSON
-func HelloWorldHandler(c echo.Context) error {
-	result := JsonResult{
-		Code: 0,
-		Data: "helloworld echo",
-	}
-	return c.JSON(http.StatusOK, result)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
