4155583f0bcf4c40d66d7fa711ea27da4fb3c658
Jinminzuo <110321926+Jinminzuo@users.noreply.github.com>
2025-09-22 11:23:46 +0800

    feat
    feat

--- service/counter_service.go
+++ service/counter_service.go
@@ -2,15 +2,7 @@ package service
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"time"
-
-	"wxcloudrun-golang/db/dao"
-	"wxcloudrun-golang/db/model"
-
-	"gorm.io/gorm"
 )
 
 // JsonResult 返回结构
@@ -20,140 +12,25 @@ type JsonResult struct {
 	Data     interface{} `json:"data"`
 }
 
-// IndexHandler 计数器接口
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := getIndex()
-	if err != nil {
-		fmt.Fprint(w, "内部错误")
+// HelloWorldHandler 返回 hello world
+func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	// 仅支持 GET
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(JsonResult{
+			Code:     -1,
+			ErrorMsg: "请求方法不支持",
+		})
 		return
 	}
-	fmt.Fprint(w, data)
-}
 
-// CounterHandler 计数器接口
-func CounterHandler(w http.ResponseWriter, r *http.Request) {
-	res := &JsonResult{}
-
-	if r.Method == http.MethodGet {
-		counter, err := getCurrentCounter()
-		if err != nil {
-			res.Code = -1
-			res.ErrorMsg = err.Error()
-		} else {
-			res.Data = counter.Count
-		}
-	} else if r.Method == http.MethodPost {
-		count, err := modifyCounter(r)
-		if err != nil {
-			res.Code = -1
-			res.ErrorMsg = err.Error()
-		} else {
-			res.Data = count
-		}
-	} else {
-		res.Code = -1
-		res.ErrorMsg = fmt.Sprintf("请求方法 %s 不支持", r.Method)
+	// 构建返回结果
+	result := JsonResult{
+		Code: 0,
+		Data: "helloworld",
 	}
 
-	msg, err := json.Marshal(res)
-	if err != nil {
-		fmt.Fprint(w, "内部错误")
-		return
-	}
-	w.Header().Set("content-type", "application/json")
-	w.Write(msg)
-}
-
-// modifyCounter 更新计数，自增或者清零
-func modifyCounter(r *http.Request) (int32, error) {
-	action, err := getAction(r)
-	if err != nil {
-		return 0, err
-	}
-
-	var count int32
-	if action == "inc" {
-		count, err = upsertCounter(r)
-		if err != nil {
-			return 0, err
-		}
-	} else if action == "clear" {
-		err = clearCounter()
-		if err != nil {
-			return 0, err
-		}
-		count = 0
-	} else {
-		err = fmt.Errorf("参数 action : %s 错误", action)
-	}
-
-	return count, err
-}
-
-// upsertCounter 更新或修改计数器
-func upsertCounter(r *http.Request) (int32, error) {
-	currentCounter, err := getCurrentCounter()
-	var count int32
-	createdAt := time.Now()
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return 0, err
-	} else if err == gorm.ErrRecordNotFound {
-		count = 1
-		createdAt = time.Now()
-	} else {
-		count = currentCounter.Count + 1
-		createdAt = currentCounter.CreatedAt
-	}
-
-	counter := &model.CounterModel{
-		Id:        1,
-		Count:     count,
-		CreatedAt: createdAt,
-		UpdatedAt: time.Now(),
-	}
-	err = dao.Imp.UpsertCounter(counter)
-	if err != nil {
-		return 0, err
-	}
-	return counter.Count, nil
-}
-
-func clearCounter() error {
-	return dao.Imp.ClearCounter(1)
-}
-
-// getCurrentCounter 查询当前计数器
-func getCurrentCounter() (*model.CounterModel, error) {
-	counter, err := dao.Imp.GetCounter(1)
-	if err != nil {
-		return nil, err
-	}
-
-	return counter, nil
-}
-
-// getAction 获取action
-func getAction(r *http.Request) (string, error) {
-	decoder := json.NewDecoder(r.Body)
-	body := make(map[string]interface{})
-	if err := decoder.Decode(&body); err != nil {
-		return "", err
-	}
-	defer r.Body.Close()
-
-	action, ok := body["action"]
-	if !ok {
-		return "", fmt.Errorf("缺少 action 参数")
-	}
-
-	return action.(string), nil
-}
-
-// getIndex 获取主页
-func getIndex() (string, error) {
-	b, err := ioutil.ReadFile("./index.html")
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
+	// 设置响应头
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
 }
